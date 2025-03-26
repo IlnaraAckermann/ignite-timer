2eaa613c6aa974c87dc2c7aeaf32f2e61871df8a
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2025-03-26 19:20:59 -0300

    feat: implement Home page with form and countdown functionality

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../../components/Header";
 
 import { LayoutContainer } from "./styles";
+import { Header } from "../../components/Header";
 
 export function DefaultLayout() {
 	return (
