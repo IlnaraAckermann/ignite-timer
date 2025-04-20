3499f336a59b7324489b0d99d2a63eaec963d6c8
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2025-04-20 13:10:55 -0300

    feat: update History component to dynamically display cycles from context

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,10 @@
+import { formatDistanceToNow } from "date-fns";
+import { useCycleContext } from "../../contexts/CycleContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
+import { ptBR } from "date-fns/locale";
 
 export function History() {
+	const { cycles } = useCycleContext();
 	return (
 		<HistoryContainer>
 			<h1>Meu histórico</h1>
@@ -16,62 +20,29 @@ export function History() {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="yellow">Em andamento</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="red">Interrompido</Status>
-							</td>
-						</tr>
+						{cycles.map((cycle) => (
+							<tr key={cycle.id}>
+								<td>{cycle.task}</td>
+								<td>{cycle.minutesAmount} minutos</td>
+								<td>
+									{formatDistanceToNow(cycle.startDate, {
+										addSuffix: true,
+										locale: ptBR,
+									})}
+								</td>
+								<td>
+									{cycle.finishDate && (
+										<Status statusColor="green">Concluído</Status>
+									)}
+									{cycle.interruptedDate && (
+										<Status statusColor="red">Interrompido</Status>
+									)}
+									{!cycle.finishDate && !cycle.interruptedDate && (
+										<Status statusColor="yellow">Em andamento</Status>
+									)}
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</HistoryList>
